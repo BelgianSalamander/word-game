59c98fa314eea251d844c96f80847144c10d3bad
Anatol Coen <anatol.coen@gmail.com>
2023-10-13 15:45:45 +1300

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,274 @@
+use std::fs::{self, File};
+use std::path::{PathBuf, Path};
+use std::str::Lines;
+use std::time::Instant;
+
+use ggez::conf::{WindowSetup, WindowMode};
+use ggez::glam::Vec2;
+use ggez::input::keyboard::KeyMods;
+use ggez::winit::event::VirtualKeyCode;
+use ggez::{Context, ContextBuilder, GameResult};
+use ggez::graphics::{self, Color, FontData, Text, Rect, StrokeOptions, Canvas, Drawable, TextFragment};
+use ggez::event::{self, EventHandler};
+use network::connect;
+use rand::Rng;
+
+pub mod network;
+
+fn main() {
+    //let mut conn = connect().unwrap();
+
+    // Make a Context.
+    let (mut ctx, event_loop) = ContextBuilder::new("my_game", "Cool Game Author")
+        .window_mode(
+            WindowMode::default()
+                .resizable(true)   
+        )
+        .build()
+        .expect("aieee, could not create ggez context!");
+
+    // Create an instance of your event handler.
+    // Usually, you should provide it with the Context object to
+    // use when setting your game up.
+    let my_game = WordGame::new(&mut ctx, "basic");
+
+    // Run!
+    event::run(ctx, event_loop, my_game);
+}
+
+struct WordGame {
+    word_list: Vec<String>,
+
+    current_words: Vec<String>,
+    received_words: Vec<String>,
+
+    last_new_word: Instant,
+
+    current_text: String
+}
+
+impl WordGame {
+    pub fn new(ctx: &mut Context, word_list: &str) -> WordGame {
+        ctx.fs.mount(Path::new("./res"), true);
+
+        let words = std::io::read_to_string(ctx.fs.open(format!("/words/{word_list}.txt")).unwrap()).unwrap();
+        let words: Lines = words.lines();
+        let words = words.filter_map(|s| {
+            let trimmed = s.trim();
+            if trimmed.len() == 0 { return None; }
+
+            let first_char: String = trimmed.chars().nth(0).unwrap().to_uppercase().collect();
+            let rest: String = trimmed.chars().skip(1).flat_map(|c| c.to_lowercase()).collect();
+
+            Some(format!("{}{}", first_char, rest))
+        }).collect();
+
+        for word in &words {
+            println!("{}", word);
+        }
+
+
+        ctx.gfx.add_font("courier_new", FontData::from_path(ctx, "/font/cour.ttf").unwrap());
+
+        WordGame {
+            word_list: words,
+
+            current_words: vec![],
+            received_words: vec![],
+
+            last_new_word: Instant::now(),
+
+            current_text: String::new()
+        }
+    }
+
+    fn add_new_word(&mut self) {
+        let mut rng = rand::thread_rng();
+
+        let idx = rng.gen_range(0..self.word_list.len());
+        self.current_words.push(self.word_list[idx].clone());
+    }
+}
+
+fn shrink(rect: Rect, v: f32) -> Rect {
+    Rect { x: rect.x + v, y: rect.y + v, w: rect.w - 2.0 * v, h: rect.h - 2.0 * v }
+}
+
+fn cut_bottom(rect: Rect, height: f32) -> (Rect, Rect) {
+    (
+        Rect {x: rect.x, y: rect.y, w: rect.w, h: rect.h - height},
+        Rect {x: rect.x, y: rect.y + rect.h - height, w: rect.w, h: height}
+    )
+}
+
+fn cut_top(rect: Rect, height: f32) -> (Rect, Rect) {
+    (
+        Rect {x: rect.x, y: rect.y, w: rect.w, h: height},
+        Rect {x: rect.x, y: rect.y + height, w: rect.w, h: rect.h - height}
+    )
+}
+
+fn cut_left(rect: Rect, width: f32) -> (Rect, Rect) {
+    (
+        Rect {x: rect.x, y: rect.y, w: width, h: rect.h},
+        Rect {x: rect.x + width, y: rect.y, w: rect.w - width, h: rect.h}
+    )
+}
+
+fn render_words_in_rect(ctx: &mut Context, canvas: &mut Canvas, words: &Vec<String>, rect: Rect, font: &str, font_size: f32, cross_out: &str) {
+    let prev = canvas.scissor_rect();
+    canvas.set_scissor_rect(rect).unwrap();
+
+    let word_width: f32 = font_size * 6.0;
+    let word_height: f32 = font_size * 0.75;
+
+    let num_columns = ((rect.w / word_width).floor() as i32).max(1);
+
+    let texts: Vec<(Text, &String)> = words.iter().map(move |w| {
+        let mut text = Text::new(w.clone());
+        text.set_font(font);
+        text.set_scale(font_size);
+        (text, w)
+    }).collect();
+
+    let column_width = rect.w / num_columns as f32;
+
+    let mut x = rect.x;
+    let mut y = rect.y;
+
+    let cross_out_lower = cross_out.to_lowercase();
+
+    for (text, word) in texts {
+        let end_y = y + word_height;
+
+        if end_y > rect.y + rect.h {
+            y = rect.y;
+            x += word_width;
+        }
+
+        canvas.draw(&text, Vec2::new(x, y));
+
+        if word.to_lowercase().starts_with(&cross_out_lower) && cross_out_lower.len() > 0 {
+            let positions = text.glyph_positions(ctx).unwrap();
+            let dimensions = text.dimensions(ctx).unwrap();
+            
+            let cross_out_len = if cross_out_lower.len() == word.len() {
+                dimensions.w
+            } else {
+                positions[cross_out_lower.len()].x
+            };
+
+            canvas.draw(
+                &graphics::Mesh::new_line(
+                    ctx,
+                    &[
+                        Vec2::new(0.0, dimensions.h * 0.5),
+                        Vec2::new(cross_out_len, dimensions.h * 0.5)
+                    ],
+                    5.0,
+                    Color::GREEN
+                ).unwrap(),
+                Vec2::new(x, y)
+            );
+        }
+
+        y += word_height;
+    }
+
+    canvas.set_scissor_rect(prev).unwrap();
+}
+
+fn center_text_in_rect(ctx: &mut Context, canvas: &mut Canvas, text: &Text, rect: Rect) {
+    let prev = canvas.scissor_rect();
+    canvas.set_scissor_rect(rect).unwrap();
+
+    let dimensions = text.dimensions(ctx).unwrap();
+
+    let x = (rect.w - dimensions.w) / 2.0;
+    let y = (rect.h - dimensions.h) / 2.0;
+
+    canvas.draw(text, Vec2::new(rect.x + x, rect.y + y));
+
+    canvas.set_scissor_rect(prev).unwrap();
+}
+
+impl EventHandler for WordGame {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        if (Instant::now() - self.last_new_word).as_secs_f32() > 1.0 {
+            self.add_new_word();
+            self.last_new_word = Instant::now();
+        }
+
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        let mut canvas = graphics::Canvas::from_frame(ctx, Color::from_rgb(100, 100, 100));
+        let draw_region = canvas.scissor_rect();
+
+        const MARGIN: f32 = 10.0;
+
+        let draw_region = shrink(draw_region, MARGIN);
+
+        let (word_region, write_region) = cut_bottom(draw_region, 100.0);
+
+        center_text_in_rect(ctx, &mut canvas, &Text::new(
+            TextFragment::new(&self.current_text)
+                .color(Color::GREEN)
+                .scale(70.0)
+                .font("courier_new")
+        ), write_region);
+
+        let (current_word_region, received_word_region) = cut_left(word_region, word_region.w * 0.5);
+        let current_word_region = shrink(current_word_region, MARGIN);
+        let received_word_region = shrink(received_word_region, MARGIN);
+
+        let (current_word_region_header, current_word_region) = cut_top(current_word_region, 80.0);
+        let (received_word_region_header, received_word_region) = cut_top(received_word_region, 80.0);
+
+        center_text_in_rect(ctx, &mut canvas, &Text::new(
+            TextFragment::new("Ollie's Corn Farm")
+                .color(Color::BLACK)
+                .scale(80.0)
+                .font("courier_new")
+        ), current_word_region_header);
+
+        center_text_in_rect(ctx, &mut canvas, &Text::new(
+            TextFragment::new("Ollie's Corn Farm")
+                .color(Color::BLACK)
+                .scale(80.0)
+                .font("courier_new")
+        ), received_word_region_header);
+
+        render_words_in_rect(ctx, &mut canvas, &self.current_words, current_word_region, "courier_new", 50.0, &self.current_text);
+
+        // Draw code here...
+        canvas.finish(ctx)
+    }
+
+    fn text_input_event(&mut self, _ctx: &mut Context, character: char) -> Result<(), ggez::GameError> {
+        if character.is_alphabetic() || character == ' ' {
+            self.current_text.push(character);
+        }
+
+        Ok(())
+    }
+
+    fn key_down_event(
+            &mut self,
+            _ctx: &mut Context,
+            input: ggez::input::keyboard::KeyInput,
+            _repeated: bool,
+        ) -> Result<(), ggez::GameError> {
+        println!("Key Pressed! {:?}", input);
+        if input.mods.intersects(KeyMods::all() & KeyMods::SHIFT.complement()) {
+            return Ok(())
+        }
+
+        if input.keycode.unwrap() == VirtualKeyCode::Back {
+            self.current_text.pop();
+        }
+
+        Ok(())
+    }
+}
---
+++ src/network.rs
@@ -0,0 +1,417 @@
+use std::net::{TcpStream, TcpListener};
+use std::io::{self, Read, ErrorKind, Write};
+use std::time::{Duration, Instant};
+
+type VersionType = u16;
+const MAJOR_VERSION: VersionType = 3;
+const MINOR_VERSION: VersionType = 5;
+
+trait FriendlyRead {
+    fn read_u8(&mut self) -> io::Result<u8>;
+    fn read_u16(&mut self) -> io::Result<u16>;
+    fn read_u32(&mut self) -> io::Result<u32>;
+    fn read_u64(&mut self) -> io::Result<u64>;
+
+    fn read_i8(&mut self) -> io::Result<i8>;
+    fn read_i16(&mut self) -> io::Result<i16>;
+    fn read_i32(&mut self) -> io::Result<i32>;
+    fn read_i64(&mut self) -> io::Result<i64>;
+    
+    fn read_f32(&mut self) -> io::Result<f32>;
+    fn read_f64(&mut self) -> io::Result<f64>;
+
+    fn read_string(&mut self) -> io::Result<String>;
+}
+
+impl<T: Read> FriendlyRead for T {
+    fn read_u8(&mut self) -> io::Result<u8> {
+        let mut buf = [0u8; 1];
+        self.read_exact(&mut buf)?;
+        Ok(u8::from_be_bytes(buf))
+    }
+
+    fn read_u16(&mut self) -> io::Result<u16> {
+        let mut buf = [0u8; 2];
+        self.read_exact(&mut buf)?;
+        Ok(u16::from_be_bytes(buf))
+    }
+
+    fn read_u32(&mut self) -> io::Result<u32> {
+        let mut buf = [0u8; 4];
+        self.read_exact(&mut buf)?;
+        Ok(u32::from_be_bytes(buf))
+    }
+
+    fn read_u64(&mut self) -> io::Result<u64> {
+        let mut buf = [0u8; 8];
+        self.read_exact(&mut buf)?;
+        Ok(u64::from_be_bytes(buf))
+    }
+
+    fn read_i8(&mut self) -> io::Result<i8> {
+        let mut buf = [0u8; 1];
+        self.read_exact(&mut buf)?;
+        Ok(i8::from_be_bytes(buf))
+    }
+
+    fn read_i16(&mut self) -> io::Result<i16> {
+        let mut buf = [0u8; 2];
+        self.read_exact(&mut buf)?;
+        Ok(i16::from_be_bytes(buf))
+    }
+
+    fn read_i32(&mut self) -> io::Result<i32> {
+        let mut buf = [0u8; 4];
+        self.read_exact(&mut buf)?;
+        Ok(i32::from_be_bytes(buf))
+    }
+
+    fn read_i64(&mut self) -> io::Result<i64> {
+        let mut buf = [0u8; 8];
+        self.read_exact(&mut buf)?;
+        Ok(i64::from_be_bytes(buf))
+    }
+
+    fn read_f32(&mut self) -> io::Result<f32> {
+        let mut buf = [0u8; 4];
+        self.read_exact(&mut buf)?;
+        Ok(f32::from_be_bytes(buf))
+    }
+
+    fn read_f64(&mut self) -> io::Result<f64> {
+        let mut buf = [0u8; 8];
+        self.read_exact(&mut buf)?;
+        Ok(f64::from_be_bytes(buf))
+    }
+
+    fn read_string(&mut self) -> io::Result<String> {
+        let length = self.read_u32()? as usize;
+        
+        let mut bytes = vec![];
+        bytes.resize(length, 0);
+        self.read(&mut bytes)?;
+
+        match String::from_utf8(bytes) {
+            Ok(x) => Ok(x),
+            Err(e) => Err(io::Error::new(io::ErrorKind::Other, format!("{:?}", e)))
+        }
+    }
+}
+
+trait FriendlyWrite {
+    fn write_u8(&mut self, x: u8) -> io::Result<()>;
+    fn write_u16(&mut self, x: u16) -> io::Result<()>;
+    fn write_u32(&mut self, x: u32) -> io::Result<()>;
+    fn write_u64(&mut self, x: u64) -> io::Result<()>;
+
+    fn write_i8(&mut self, x: i8) -> io::Result<()>;
+    fn write_i16(&mut self, x: i16) -> io::Result<()>;
+    fn write_i32(&mut self, x: i32) -> io::Result<()>;
+    fn write_i64(&mut self, x: i64) -> io::Result<()>;
+    
+    fn write_f32(&mut self, x: f32) -> io::Result<()>;
+    fn write_f64(&mut self, x: f64) -> io::Result<()>;
+
+    fn write_string(&mut self, x: &str) -> io::Result<()>;
+}
+
+impl<T: Write> FriendlyWrite for T {
+    fn write_u8(&mut self, x: u8) -> io::Result<()> {
+        self.write_all(&x.to_be_bytes())
+    }
+
+    fn write_u16(&mut self, x: u16) -> io::Result<()> {
+        self.write_all(&x.to_be_bytes())
+    }
+
+    fn write_u32(&mut self, x: u32) -> io::Result<()> {
+        self.write_all(&x.to_be_bytes())
+    }
+
+    fn write_u64(&mut self, x: u64) -> io::Result<()> {
+        self.write_all(&x.to_be_bytes())
+    }
+
+    fn write_i8(&mut self, x: i8) -> io::Result<()> {
+        self.write_all(&x.to_be_bytes())
+    }
+
+    fn write_i16(&mut self, x: i16) -> io::Result<()> {
+        self.write_all(&x.to_be_bytes())
+    }
+
+    fn write_i32(&mut self, x: i32) -> io::Result<()> {
+        self.write_all(&x.to_be_bytes())
+    }
+
+    fn write_i64(&mut self, x: i64) -> io::Result<()> {
+        self.write_all(&x.to_be_bytes())
+    }
+
+    fn write_f32(&mut self, x: f32) -> io::Result<()> {
+        self.write_all(&x.to_be_bytes())
+    }
+
+    fn write_f64(&mut self, x: f64) -> io::Result<()> {
+        self.write_all(&x.to_be_bytes())
+    }
+
+    fn write_string(&mut self, x: &str) -> io::Result<()> {
+        self.write_all(&x.len().to_be_bytes())?;
+        self.write_all(x.as_bytes())?;
+
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+pub enum Packet {
+    ClientInfo { // 0
+        major: VersionType,
+        minor: VersionType
+    }
+}
+
+impl Packet {
+    pub fn parse<T: Read>(mut data: T) -> io::Result<Self> {
+        let packet_type = data.read_u16()?;
+
+        let packet = match packet_type {
+            0 => Self::ClientInfo { 
+                major: data.read_u16()?, 
+                minor: data.read_u16()?
+            },
+
+            x => {
+                return Err(io::Error::new(io::ErrorKind::Other, format!("Unrecognised packet type {}", x)));
+            }
+        };
+
+        Ok(packet)
+    }
+
+    fn packet_id(&self) -> u16 {
+        match self {
+            Self::ClientInfo {..} => 0
+        }
+    }
+
+    pub fn write<T: Write>(&self, mut out: T) -> io::Result<()> {
+        out.write_u16(self.packet_id())?;
+
+        match self {
+            Self::ClientInfo { major, minor } => {
+                out.write_u16(*major)?;
+                out.write_u16(*minor)?;
+            }
+        }
+
+        Ok(())
+    }
+
+    pub fn client_info() -> Packet {
+        Self::ClientInfo { major: MAJOR_VERSION, minor: MINOR_VERSION }
+    }
+}
+
+pub struct Connection {
+    stream: TcpStream,
+    buf: Vec<u8>,
+    buf_pos: usize
+}
+
+pub struct DebugWriteWrapper<T: Write> {
+    inner: T
+}
+
+impl<T: Write> DebugWriteWrapper<T> {
+    pub fn new(inner: T) -> Self {
+        DebugWriteWrapper { inner }
+    }
+}
+
+impl<T: Write> Write for DebugWriteWrapper<T> {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        print!("Writing the following data: '");
+
+        for x in buf {
+            print!("{}", x);
+        }
+
+        println!("'");
+
+        self.inner.write(buf)
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.inner.flush()
+    }
+}
+
+impl Connection {
+    const CHUNK_SIZE: usize = 8192;
+
+    pub fn new(stream: TcpStream) -> io::Result<Self> {
+        stream.set_nonblocking(true)?;
+        Ok(Connection {
+            stream,
+            buf: vec![],
+            buf_pos: 0
+        })
+    }
+
+    fn try_read(&mut self, bytes: usize) -> io::Result<usize> {
+        if self.buf_pos + bytes >= self.buf.len() {
+            self.buf.resize(self.buf.len() + Self::CHUNK_SIZE, 0);
+        }
+
+        match self.stream.read(&mut self.buf[self.buf_pos..self.buf_pos + bytes]) {
+            Ok(x) => {
+                self.buf_pos += x;
+                Ok(x)
+            },
+            Err(e) if e.kind() == ErrorKind::WouldBlock => Ok(0),
+            Err(e) => Err(e)
+        }
+    }
+
+    pub fn poll_next_packet(&mut self) -> io::Result<Option<Packet>> {
+        while self.buf_pos < 4 {
+            if self.try_read(4 - self.buf_pos)? == 0 {
+                return Ok(None);
+            }
+        }
+
+        let packet_size = u32::from_be_bytes([self.buf[0], self.buf[1], self.buf[2], self.buf[3]]);
+        println!("Attempting to get packet of {} bytes", packet_size);
+        let mut required = packet_size as usize - self.buf_pos + 4;
+
+        while required > 0 {
+            println!("Missing {} bytes", required);
+
+            let n_read = self.try_read(required)?;
+
+            if n_read == 0 {
+                return Ok(None);
+            }
+
+            required -= n_read;
+        }
+
+        let res = Packet::parse(&self.buf[4..])?;
+
+        self.buf_pos = 0;
+        println!("Received packet!");
+        println!("{:?}", res);
+
+        Ok(Some(res))
+    }
+
+    pub fn next_packet(&mut self, timeout: Duration) -> io::Result<Packet> {
+        let start = Instant::now();
+
+        while (Instant::now() - start) < timeout {
+            match self.poll_next_packet()? {
+                Some(x) => return Ok(x),
+                _ => {}
+            }
+        }
+
+        return Err(io::Error::new(io::ErrorKind::TimedOut, "Timed out on Connection::next_packet"));
+    }
+
+    pub fn send_packet(&mut self, packet: Packet) -> io::Result<()> {
+        let mut data = vec![];
+        packet.write(DebugWriteWrapper::new(&mut data))?;
+        self.stream.write_u32(data.len() as _)?;
+        self.stream.write_all(&data)
+    }
+}
+
+fn get_yes_no(msg: &str) -> io::Result<bool> {
+    let stdin = io::stdin();
+
+    loop {
+        println!("{} (Y/n)", msg);
+
+        let mut buffer = String::new();
+        stdin.read_line(&mut buffer)?;
+        buffer = buffer.trim().to_string();
+
+        if buffer.len() != 1 {
+            println!("Only answer with a single character");
+        } else {
+            let c = buffer.chars().nth(0).unwrap();
+
+            if c == 'y' || c == 'Y' {
+                return Ok(true);
+            } else if c == 'n' || c == 'N' {
+                return Ok(false);
+            } else {
+                println!("Answer with 'y','Y','n', or 'N'");
+            }
+        }
+    }
+}
+
+fn pair_up() -> io::Result<TcpStream> {
+    println!("Finding Connection");
+    let stdin = io::stdin();
+
+    let host = get_yes_no("Do you want to host?")?;
+
+    let mut ip = String::new();
+    println!("Enter IP:");
+    stdin.read_line(&mut ip)?;
+    let ip = ip.trim().to_string();
+
+    let mut port = String::new();
+    println!("Enter Port:");
+    stdin.read_line(&mut port)?;
+    let port = match port.trim().parse() {
+        Ok(x) => x,
+        Err(e) => return io::Result::Err(io::Error::new(io::ErrorKind::Other, format!("{:?}", e)))
+    };
+
+
+    if host {
+        println!("Waiting for connection on {}:{}", ip, port);
+
+        let listener = TcpListener::bind((ip, port))?;
+        let (stream, addr) = listener.accept()?;
+
+        println!("Got connection from {}", addr);
+
+        Ok(stream)
+    } else {
+        println!("Attempting to connect to {}:{}", ip, port);
+
+        let res = TcpStream::connect((ip, port))?;
+
+        println!("Connected!");
+
+        Ok(res)
+    }
+}
+
+pub fn connect() -> io::Result<Connection> {
+    let mut conn = Connection::new(pair_up()?)?;
+    
+    conn.send_packet(Packet::client_info())?;
+
+    match conn.next_packet(Duration::from_secs(10))? {
+        Packet::ClientInfo { major, minor } => {
+            if major != MAJOR_VERSION && minor != MINOR_VERSION {
+                return Err(io::Error::new(io::ErrorKind::Other, format!("Trying to connect to client with mismatched version. Your version = {}.{}. Their version = {}.{}.", MAJOR_VERSION, MINOR_VERSION, major, minor)));
+            }
+        },
+
+        packet => {
+            return Err(io::Error::new(io::ErrorKind::Other, format!("First packet should be a ClientInfo packet. Received {:?}", packet)));
+        }
+    }
+
+    println!("Verified versions!");
+
+    Ok(conn)
+}
